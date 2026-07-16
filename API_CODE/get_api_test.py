a080fd617aef2034c36a9319e2367a5364979ba4
Sun Jiaxiang <s1040813559@outlook.com>
2026-07-16 15:49:44 +0800

    get查询用户名

--- API_CODE/get_api_test.py
+++ API_CODE/get_api_test.py
@@ -1,22 +1,45 @@
 import requests
 from random import randint
 
+# url = "https://api.github.com/search/users"
+# url += "?q=jax+in:login"
+
+# headers = {"Accept":"application/vnd.github+json"}
+
+# r = requests.get(url,headers=headers)
+# print(f"status code {r.status_code}")
+
+# dict1 = r.json()
+# print(f"total count - {dict1['total_count']}\nincomplete_result {dict1['incomplete_results']}")
+
+
+# all_item = dict1["items"]
+
+# for i in range(3):
+#     # print(all_item[i])
+#     for key,value in all_item[i].items():
+#         print(f"{key} - {value}")
+#     print("\n")
+
+
+
+#获取我自己的github的主页api（通过get查找用户）
 url = "https://api.github.com/search/users"
-url += "?q=jax+in:login"
+url += "?q=Nia-keep-studying+in:login"
 
 headers = {"Accept":"application/vnd.github+json"}
+response = requests.get(url,headers=headers)
+print(response.status_code)
 
-r = requests.get(url,headers=headers)
-print(f"status code {r.status_code}")
+repo_dict = response.json()
+print(repo_dict.keys())
 
-dict1 = r.json()
-print(f"total count - {dict1['total_count']}\nincomplete_result {dict1['incomplete_results']}")
+# print(f"totalcount:{repo_dict["total_count"]}\nresults{repo_dict["incomplete_results"]}")
 
+user = repo_dict["items"][0]
 
-all_item = dict1["items"]
-
-for i in range(3):
-    # print(all_item[i])
-    for key,value in all_item[i].items():
-        print(f"{key} - {value}")
-    print("\n")
+print(
+    f"login: {user['login']}\n"
+    f"id: {user['id']}\n"
+    f"url: {user['url']}"
+)
